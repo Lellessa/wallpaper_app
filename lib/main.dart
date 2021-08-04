2552a86bfafe1e1b23a3c3ddc13d31d41ac114c2
lellessa <lessa.leo20@gmail.com>
2021-08-03 22:35:45 -0300

    feat: Configurando firebase no app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:wallpaper_app/pages/home.dart';
 
 void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  
   runApp(MyApp());
   await SystemChrome.setEnabledSystemUIOverlays([]);
 }
